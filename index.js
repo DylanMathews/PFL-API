8c3ec1949e2a7ea4b45c81fc9d0a845229404e1d
Dylan Mathews <Dylanmathews@gmail.com>
2020-08-26 09:22:13 -0600

    Added pokemon route as example.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
-const {db} = require('./resources/resources')
+const {db, User, Pokemon, Account} = require('./resources/resources')
+
+const PORT = 4000
 
 app.get('/', (req, res) => {
 	res.json("Hello");
 })
 
-app.listen(4000, () => {
-	console.log("Listening on port 4000!")
+app.get('/pokemon', async (req, res) => {
+	// explicitly check for userId query
+	try {
+		const {userId} = req.query
+		const params = {userId} // This way, params has only known keys. DON'T TRUST THE FRONTEND!
+		const pokemon = Pokemon.findAll({where: params})
+
+		res.status(200).json(pokemon)
+	} catch (e) {
+		console.error(e)
+		res.status(400).json({error: "Could not find pokemon."})
+	}
+})
+
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}!`)
 })
